b7a166e327d5b4ab1e6947708fc7ba7834661102
Shagon <goshan58rus@gmail.com>
2021-04-09 22:07:32 +0300

    Обновил

--- executable/src/main.rs
+++ executable/src/main.rs
@@ -6,33 +6,7 @@ mod helper_traits;
 
 #[tokio::main]
 async fn main() {
-    let dsn_1 = "postgres://sqlx_user:sqlx_pass@localhost:5656/sqlx_work_please";
-    let pool = sqlx::postgres::PgPoolOptions::new()
-        .max_connections(25)
-        .min_connections(5)
-        .connect_timeout(tokio::time::Duration::from_secs(10))
-        .connect(dsn_1)
-        .await
-        .unwrap();
-
-    let mut _executor_1 = pool.acquire().await.unwrap();
-
-    //
-
-    let dsn_2 = "postgresql://";
-    let pool = sqlx::postgres::PgPoolOptions::new()
-        .max_connections(25)
-        .min_connections(5)
-        .connect_timeout(tokio::time::Duration::from_secs(10))
-        .connect(dsn_2)
-        .await
-        .unwrap();
-
-    let mut _executor_2 = pool.acquire().await.unwrap();
-
-    //
-
-    let dsn_3 = "postgresql://12312;fads;afdsv";
+    let dsn_3 = "postgresql://";
     let pool = sqlx::postgres::PgPoolOptions::new()
         .max_connections(25)
         .min_connections(5)
