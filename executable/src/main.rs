393ab65bf3ee1d28da7482c243afc3f8222cc7ee
Shagon <goshan58rus@gmail.com>
2021-04-09 21:51:15 +0300

    Обновил

--- executable/src/main.rs
+++ executable/src/main.rs
@@ -4,88 +4,42 @@ use proc_macro_function_like::*;
 extern crate const_format;
 mod helper_traits;
 
-fn main() {
-    println!("Hello, world!");
-}
-
-#[tokio::test]
-async fn test() {
-    use serde::{Deserialize, Serialize};
-    use sqlx::FromRow;
-
-    #[derive(Debug, Clone, Serialize, Deserialize, FromRow)]
-    pub struct User {
-        pub id: i32,
-        pub login: String,
-        pub email: Option<String>,
-    }
-
-    impl User {
-        pub const TABLE_NAME: &'static str = "users";
-    }
-
-    // что душе угодно
-    let dsn = "postgresql://";
-    // let dsn = "postgres://sqlx_user:sqlx_pass@localhost:5656/sqlx_work_please";
+#[tokio::main]
+async fn main() {
+    let dsn_1 = "postgres://sqlx_user:sqlx_pass@localhost:5656/sqlx_work_please";
     let pool = sqlx::postgres::PgPoolOptions::new()
         .max_connections(25)
         .min_connections(5)
         .connect_timeout(tokio::time::Duration::from_secs(10))
-        .connect(dsn)
+        .connect(dsn_1)
         .await
         .unwrap();
 
-    let mut executor = pool.acquire().await.unwrap();
-    // let login_var = "my_login".to_string();
-    // let email_var = "example@example.com".to_string();
+    let mut _executor_1 = pool.acquire().await.unwrap();
+
     //
-    // let s = pg_query!(User, &mut executor, login = login_var, hourly_rate = Some(10));
-    // let s2 = pg_query!(User, &mut executor, login = login_var, hourly_rate = 10.0);
-    // let s3 = pg_query!(User, &mut executor, login = login_var, hourly_rate = Some(10.0));
-    // // dbg!(s);
+
+    let dsn_2 = "postgresql://";
+    let pool = sqlx::postgres::PgPoolOptions::new()
+        .max_connections(25)
+        .min_connections(5)
+        .connect_timeout(tokio::time::Duration::from_secs(10))
+        .connect(dsn_2)
+        .await
+        .unwrap();
+
+    let mut _executor_2 = pool.acquire().await.unwrap();
+
     //
-    //
-    // let f = sqlx::query_as!(
-    //     User,
-    //     r#"SELECT id, avatar_url, login, email, firstname, lastname, description, hourly_rate,
-    //     currency, created_at, state as "state: _", role as "role: _" FROM users WHERE login = $1 AND hourly_rate = $2"#,
-    //     "rest",
-    //     Some(10.0_f64)
-    // )
-    //     .fetch_optional(&mut executor)
-    //     .await.unwrap();
-    // dbg!(f);
-    //
-    // let f2 = sqlx::query_as!(
-    //     User,
-    //     r#"SELECT id, avatar_url, login, email, firstname, lastname, description, hourly_rate,
-    //     currency, created_at, state as "state: _", role as "role: _" FROM users WHERE login = $1 AND hourly_rate = $2"#,
-    //     "rest",
-    //     10.0_f64
-    // )
-    //     .fetch_optional(&mut executor)
-    //     .await.unwrap();
-    // dbg!(f2);
-    //
-    // let f3 = sqlx::query_as!(
-    //     User,
-    //     r#"SELECT id, avatar_url, login, email, firstname, lastname, description, hourly_rate,
-    //     currency, created_at, state as "state: _", role as "role: _" FROM users WHERE login = $1 AND hourly_rate = $2"#,
-    //     "rest",
-    //     10_f64
-    // )
-    //     .fetch_optional(&mut executor)
-    //     .await.unwrap();
-    // dbg!(f3);
-    //
-    // let f4 = sqlx::query_as!(
-    //     User,
-    //     r#"SELECT id, avatar_url, login, email, firstname, lastname, description, hourly_rate, currency, created_at,
-    //     state as "state: _", role as "role: _" FROM users WHERE login = $1 AND email = $2"#,
-    //     "rest",
-    //     "123"
-    // )
-    //     .fetch_optional(&mut executor)
-    //     .await.unwrap();
-    // dbg!(f4);
+
+    let dsn_3 = "postgresql://12312;fads;afdsv";
+    let pool = sqlx::postgres::PgPoolOptions::new()
+        .max_connections(25)
+        .min_connections(5)
+        .connect_timeout(tokio::time::Duration::from_secs(10))
+        .connect(dsn_3)
+        .await
+        .unwrap();
+
+    let mut _executor_3 = pool.acquire().await.unwrap();
 }
