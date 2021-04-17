14c6b64876c58b67c04cb711c966e2872212f6e1
Shagon <goshan58rus@gmail.com>
2021-04-17 17:29:45 +0300

    Fix types

--- executable/src/main.rs
+++ executable/src/main.rs
@@ -4,33 +4,35 @@ mod helper_traits;
 async fn main() {
 
     mod case_1 {
+        use sqlx::Row;
+
         type Conn = sqlx::pool::PoolConnection<sqlx::Postgres>;
 
-        fn by_mut_ref(executor: &mut Conn) -> i32 {
-            let res1 = sqlx::query_as::<_, i32>("SELECT 1").fetch_one(executor).await.unwrap();
-            let res2 = sqlx::query_as::<_, i32>("SELECT 1").fetch_one(executor).await.unwrap();
+        async fn by_mut_ref(executor: &mut Conn) -> i32 {
+            let res1 = sqlx::query("SELECT 1").fetch_one(executor).await.unwrap().get::<i32, _>(0);
+            let res2 = sqlx::query("SELECT 1").fetch_one(executor).await.unwrap().get::<i32, _>(0);
             res1 + res2
         }
 
-        fn mut_move(mut executor: Conn) -> i32 {
-            let res1 = sqlx::query_as::<_, i32>("SELECT 1").fetch_one(&mut executor).await.unwrap();
-            let res2 = sqlx::query_as::<_, i32>("SELECT 1").fetch_one(&mut executor).await.unwrap();
+        async fn mut_move(mut executor: Conn) -> i32 {
+            let res1 = sqlx::query("SELECT 1").fetch_one(&mut executor).await.unwrap().get::<i32, _>(0);
+            let res2 = sqlx::query("SELECT 1").fetch_one(&mut executor).await.unwrap().get::<i32, _>(0);
             res1 + res2
         }
 
-        fn some_func_mut(mut executor: Conn) -> i32 {
-            let res3 = mut_move(executor);
-            let res1 = by_mut_ref(&mut executor);
-            let res2 = by_mut_ref(&mut executor);
-            let res4 = mut_move(executor);
+        async fn some_func_mut(mut executor: Conn) -> i32 {
+            let res3 = mut_move(executor).await;
+            let res1 = by_mut_ref(&mut executor).await;
+            let res2 = by_mut_ref(&mut executor).await;
+            let res4 = mut_move(executor).await;
             res1 + res2 + res3 + res4
         }
 
-        fn some_func_mut_ref(mut executor: &mut Conn) -> i32 {
-            let res3 = mut_move(executor);
-            let res1 = by_mut_ref(executor);
-            let res2 = by_mut_ref(executor);
-            let res4 = mut_move(executor);
+        async fn some_func_mut_ref(mut executor: &mut Conn) -> i32 {
+            let res3 = mut_move(executor).await;
+            let res1 = by_mut_ref(executor).await;
+            let res2 = by_mut_ref(executor).await;
+            let res4 = mut_move(executor).await;
             res1 + res2 + res3 + res4
         }
     }
